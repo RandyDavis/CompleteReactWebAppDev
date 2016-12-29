00c7506fac1d90990cceb427e296a96617c7cada
Randy Davis <randy_davis@outlook.com>
2016-12-29 13:12:27 -0600

    complete 'Nested Components Pt.1'

--- 03_HelloReact/public/app.jsx
+++ 03_HelloReact/public/app.jsx
@@ -1,3 +1,25 @@
+var GreeterMessage = React.createClass({
+   render: function () {
+       return (
+            <div>
+                <h1>Some H1</h1>
+                <p>Some paragraph</p>
+            </div>
+       );
+   }
+});
+
+var GreeterForm = React.createClass({
+    render: function () {
+        return (
+            <form>
+                <input type="text" ref="name" placeholder="Static Form Component"/>
+                <button>Set Name</button>
+            </form>
+        );
+    }
+});
+
 var Greeter = React.createClass({
     getDefaultProps: function () {
         return {
@@ -32,6 +54,9 @@ var Greeter = React.createClass({
             <div>
                 <h1>Hello {name}!</h1>
                 <p>{msg}</p>
+
+                <GreeterMessage />
+                <GreeterForm />
 
                 <form onSubmit={this.onButtonClick}>
                     <input type="text" ref="name"/>
