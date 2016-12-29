092e0c376bad0cebcf415642ae4164fc35bacac1
Randy Davis <randy_davis@outlook.com>
2016-12-29 01:53:03 -0600

    complete 'Component Properties'

--- 03_HelloReact/public/app.jsx
+++ 03_HelloReact/public/app.jsx
@@ -1,15 +1,26 @@
 var Greeter = React.createClass({
+    getDefaultProps: function () {
+        return {
+            name: 'React',
+            msg: 'This is from a component!'
+        };
+    },
     render: function () {
+        var name = this.props.name;
+        var msg = this.props.msg;
         return (
             <div>
-                <h1>Hello React!</h1>
-                <p>This is from a React component</p>
+                <h1>Hello {name}!</h1>
+                <p>{msg}</p>
             </div>
         );
     }
 });
 
+var firstName = 'Randy';
+var msgText = 'React component text from a message prop!';
+
 ReactDOM.render(
-    <Greeter/>,
+    <Greeter name={firstName} />,
     document.getElementById('app')
 );
