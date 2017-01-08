93e6e6b99d08af8297db50de5e809712031821c4
Randy Davis <randy_davis@outlook.com>
2017-01-08 01:27:46 -0600

    update app to use Firebase hosting; updated server.js to not use http that heroku needed and add firebase json and rc files

--- 06_DeployingAppToProd/ReactWeather/server.js
+++ 06_DeployingAppToProd/ReactWeather/server.js
@@ -2,16 +2,16 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
-      next();
-    } else {
-        res.redirect('http://' + req.hostname + req.url);
-    }
-});
+// app.use(function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] === 'http') {
+//       next();
+//     } else {
+//         res.redirect('http://' + req.hostname + req.url);
+//     }
+// });
 
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-    console.log("Server is running on port " + port);
+    console.log("Server is running on port " + PORT);
 })
