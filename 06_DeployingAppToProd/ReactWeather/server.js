cc542e486bd77fd93c15774035b93456eaa2033f
Randy Davis <randy_davis@outlook.com>
2017-01-08 03:14:11 -0600

    remove Firebase-related deployment files and update server.js to use http

--- 06_DeployingAppToProd/ReactWeather/server.js
+++ 06_DeployingAppToProd/ReactWeather/server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] === 'http') {
-//       next();
-//     } else {
-//         res.redirect('http://' + req.hostname + req.url);
-//     }
-// });
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+      next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 app.use(express.static('public'));
 
