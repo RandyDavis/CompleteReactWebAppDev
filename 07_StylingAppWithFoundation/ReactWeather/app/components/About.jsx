8a32a4c04137638b6e5e60c9e1adbb3e70341e42
Randy Davis <randy_davis@outlook.com>
2017-01-12 23:58:05 -0600

    complete 'Styling About Page'

--- 07_StylingAppWithFoundation/ReactWeather/app/components/About.jsx
+++ 07_StylingAppWithFoundation/ReactWeather/app/components/About.jsx
@@ -3,8 +3,20 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the About page!</p>
+            <h1 className="text-center">About</h1>
+            <p>React Weather is all about bringing you up to the minute weather in your area.</p>
+            <p>
+                We are powered by the following tools: <hr/>
+            </p>
+            <ul>
+                <li>
+                    <a href='https://facebook.github.io/react/'><em>ReactJS</em></a> - One of the fastest JavaScript Libraries used.
+                </li>
+                <li>
+                    <a href="http://openweathermap.org"><em>Open Weather Map</em></a> - An excellent API for all your weather-related needs.
+                </li>
+            </ul>
+
         </div>
     );
 };
