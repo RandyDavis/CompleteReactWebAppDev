e99d43a17baf8c07e19e9abde41d2ff14fa91f7c
Randy Davis <randy_davis@outlook.com>
2017-01-16 23:52:25 -0600

    complete 'Adding A Modal For Errors Pt.2'

--- 07_StylingAppWithFoundation/ReactWeather/app/components/Weather.jsx
+++ 07_StylingAppWithFoundation/ReactWeather/app/components/Weather.jsx
@@ -53,7 +53,7 @@ var Weather = React.createClass({
         function renderError () {
             if (typeof errorMessage === 'string') {
                 return (
-                    <ErrorModal />
+                    <ErrorModal message={errorMessage} />
                 )
             }
         }
